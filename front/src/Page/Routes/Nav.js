296f8a7645ed82f5bdc1e688b38a7166e4122fcb
suho <suho0815@naver.com>
2023-08-30 19:10:35 +0900

    font test

--- front/src/Page/Routes/Nav.js
+++ front/src/Page/Routes/Nav.js
@@ -3,16 +3,24 @@ import {Link} from 'react-router-dom'
 
 export function Nav() {
   return (
-    <nav className="w-full h-[50px] fixed z-50 border border-black flex justify-between items-center">
+    <nav className="w-full h-[70px] fixed z-50 border border-black flex justify-between items-center">
       <div>
         <Link to="/">
           <p>logo</p>
         </Link>
       </div>
-      <div className="flex justify-end items-center">
+      <div className="flex items-center justify-center">
         <Link to="/static">
-          <p className="w-[120px] h-full text-2xl text-center align-middle text-black">통계보기</p>
+          <p className="w-[120px] h-full text-2xl text-center align-middle text-black">통계</p>
         </Link>
+        <Link to="/result">
+          <p className="w-[120px] h-full text-2xl text-center align-middle text-black">자료 조회</p>
+        </Link>
+        <Link to="/result">
+          <p className="w-[120px] h-full text-2xl text-center align-middle text-black">고객센터</p>
+        </Link>
+      </div>
+      <div className="flex items-center justify-end">
         <Link to="/login">
           <p className="w-[120px] h-full text-2xl text-center align-middle text-black">로그인</p>
         </Link>
--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,25 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        custom: ['porter-sans', 'porter-sans-block']
+      },
       screens: {
-        "2xl": { max: "1535px" },
+        '2xl': {max: '1535px'},
         // => @media (max-width: 1535px) { ... }
 
-        xl: { max: "1279px" },
+        xl: {max: '1279px'},
         // => @media (max-width: 1279px) { ... }
 
-        lg: { max: "1023px" },
+        lg: {max: '1023px'},
         // => @media (max-width: 1023px) { ... }
 
-        md: { max: "767px" },
+        md: {max: '767px'},
         // => @media (max-width: 767px) { ... }
 
-        sm: { max: "639px" },
+        sm: {max: '639px'}
         // => @media (max-width: 639px) { ... }
-      },
-    },
+      }
+    }
   },
-  plugins: [require("@tailwindcss/line-clamp"), require("daisyui")],
-};
+  plugins: [require('@tailwindcss/line-clamp'), require('daisyui')]
+}
