353fcfe81b8bee180ef43aa0d34b19010cb2d642
Jieun Huh <entks301@naver.com>
2023-08-31 17:02:30 +0900

    'userBoard.domain'

---
+++ Back/AI_Project_K3/src/main/java/edu/pnu/domain/UserBoard.java
@@ -0,0 +1,36 @@
+package edu.pnu.domain;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "board")
+public class UserBoard {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String user_id;
+	private String title;
+	private String content;
+	private LocalDate date;
+	private LocalTime time;
+	private Integer viewcnt;
+}
