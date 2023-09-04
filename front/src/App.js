1ae125649d1ab5c3489d59b78902ca48bb2679eb
Jieun Huh <entks301@naver.com>
2023-09-04 17:56:09 +0900

    front

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,8 @@
 import { FileUpload } from './Component'
-import {Login} from './Page/LoginPage/Login'
-import {RouteMain} from './Page/Routes/RouteMain'
+import { Login } from './Page/LoginPage/Login'
+import MainPage from './Page/MainPage'
+import { RouteMain } from './Page/Routes/RouteMain'
+
 
 function App() {
   // const data = [25, 45, 60, 30, 10]
@@ -10,9 +12,11 @@ function App() {
   return (
     <div className="w-full h-full">
       {/* <Login /> */}
-      <FileUpload/>
+      {/* <FileUpload/> */}
+      <MainPage />
     </div>
   )
 }
+
 
 export default App
