7aeb0e2cb6c63310b2305b122a4f6835b19a4f1a
Jieun Huh <entks301@naver.com>
2023-09-07 17:53:03 +0900

    security config

--- Back/AI_Project_K3/src/main/java/edu/pnu/config/SecurityConfig.java
+++ Back/AI_Project_K3/src/main/java/edu/pnu/config/SecurityConfig.java
@@ -1,50 +1,82 @@
 package edu.pnu.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.io.IOException;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import edu.pnu.persistence.RecyclingRepository;
+import edu.pnu.filter.JwtAuthenticationFilter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Autowired
-    private RecyclingRepository recyclingRepo;
-
-    @Autowired
-    private AuthenticationConfiguration authConfig;
+    private final AuthenticationConfiguration authConfig;
+    
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     // Security Setting
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.csrf(abcd -> abcd.disable());
-        http.sessionManagement(abcd -> abcd.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http
+        .cors().and()
+        .csrf(abcd -> abcd.disable())
+        .httpBasic().disable()
+        .sessionManagement(abcd -> abcd.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         http.authorizeHttpRequests(authorize -> {
-            authorize
-                    .requestMatchers("/member/**").authenticated()
-                    .requestMatchers("/manager/**" ).hasAnyRole("MANAGER", "ADMIN")
-                    .requestMatchers("/admin/**").hasRole("ADMIN")
-                    // 권한 설정 추가 기억이겅 !!
-                    .requestMatchers("/image",
-                    		"/signup", "/login", 
-                    		"/fileupload", "/statistics/**",
-                    		"/board/**", "/images/**",
-                    		"/comments/**",  "/boardList").permitAll()
-                    .anyRequest().authenticated();
+            try {
+				authorize
+				        .requestMatchers("/member/**").authenticated()
+				        .requestMatchers("/manager/**", "/fileupload").hasAnyRole("MANAGER", "ADMIN")
+				        .requestMatchers("/admin/**","/fileupload").hasRole("ADMIN")
+				        // 권한 설정 추가 기억이겅 !!
+// get method 권한 인증 .requestMatchers(HttpMethod.GET, ".api URI").permitAll()
+				        .requestMatchers("/image",
+				        		"/signup", "/login", 
+				        		"/statistics/**",
+				        		"/board/**", "/images/**",
+				        		"/boardList","comments/**").permitAll()
+				        .anyRequest().authenticated()
+				        .and().exceptionHandling().authenticationEntryPoint(new FailedAuthenticationEntryPoint());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
         });
-
-        http.cors();
+        
+       http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+    
 
         return http.build();
+    }
+    
+ 
+    class FailedAuthenticationEntryPoint implements AuthenticationEntryPoint{
+
+		@Override
+		public void commence(HttpServletRequest request, HttpServletResponse response,
+				AuthenticationException authException) throws IOException, ServletException {
+			response.setContentType("application/json");
+			response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+			response.getWriter().write("{\"code\": \"NP\", \"message\":\"Do not have permission.\" }");
+		}
+    	
     }
 
     // password 암호화
@@ -53,18 +85,4 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    // @Bean
-    // public SecurityFilterChain filterChain(HttpSecurity security) throws
-    // Exception {
-    // security.authorizeHttpRequests(auth -> {
-    // auth.requestMatchers("/user/**").authenticated()
-    // .requestMatchers("/employee/**").hasAnyRole("EMPLOYEE", "MANAGER") //
-    // manager, admin만 허용
-    // .requestMatchers("/Manager/**").hasRole("MANAGER") // admin만 허용
-    // .anyRequest().permitAll();
-    // });
-
-    // return security.build();
-
-    // }
 }
