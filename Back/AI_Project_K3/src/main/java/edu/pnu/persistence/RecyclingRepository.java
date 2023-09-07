e9fdb9f876774eaabb77c1f34b1849ab8dcd6b25
Jieun Huh <entks301@naver.com>
2023-09-07 17:55:24 +0900

    repository

--- Back/AI_Project_K3/src/main/java/edu/pnu/persistence/RecyclingRepository.java
+++ Back/AI_Project_K3/src/main/java/edu/pnu/persistence/RecyclingRepository.java
@@ -1,12 +1,12 @@
 package edu.pnu.persistence;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import edu.pnu.domain.RecycleStaticsProjection;
 import edu.pnu.domain.Recycling;
 
 public interface RecyclingRepository extends JpaRepository<Recycling, String> {
@@ -16,11 +16,17 @@ public interface RecyclingRepository extends JpaRepository<Recycling, String> {
 			+ " from Recycling tr"
 			+ " join RecycleRes rl on tr.detect_log_id=rl.detect_log_id" 
 			+ " where tr.state='t' and rl.category= ?1")
-	List<RecycleStaticsProjection> findRecycleStatisticsByCategory(String category);
+	List<Object[]> findRecycleStatisticsByCategory(String category);
 	
 	@Query("select tr.rm, tr.ce, tr.date, tr.time, rl.category, rl.count"
 			+ " from Recycling tr"
 			+ " join RecycleRes rl on tr.detect_log_id=rl.detect_log_id"
-			+ " where tr.state='t' and tr.date= ?1")
-	List<RecycleStaticsProjection> findRecycleStaticsProjectionsByDate(LocalDate date);
+			+ " where tr.state='t' and tr.date between ?1 and ?2")
+	List<Object[]> findRecycleStaticsProjectionsByDate(LocalDate date1, LocalDate date2);
+	
+	@Query("select tr.rm, tr.ce, tr.date, tr.time, rl.category, rl.count" 
+			+ " from Recycling tr"
+			+ " join RecycleRes rl on tr.detect_log_id=rl.detect_log_id" 
+			+ " where tr.state='t' and tr.time between ?1 and ?2")
+	List<Object[]> findRecycleStatisticsByTime(LocalTime time1, LocalTime time2);
 }
